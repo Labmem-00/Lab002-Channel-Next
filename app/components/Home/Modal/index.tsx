99beeb7bd0f77f0516a088628798c98ef2b349d4
Labmem.00 <99648872+Labmem-00@users.noreply.github.com>
2024-12-20 18:55:58 +0800

    feat(headerBar): login

--- app/components/Home/Modal/index.tsx
+++ app/components/Home/Modal/index.tsx
@@ -1,19 +1,28 @@
-'use client'
+'use client';
 
 import React from 'react';
 import { useModalStore } from '@/store/temp/modal';
 import componentRegistry from './registry';
+import styles from './modal.module.scss';
+import { Icon } from '@iconify/react/dist/iconify.js';
 
 const Modal: React.FC = () => {
-  const { isOpen, componentKey } = useModalStore();
-
+  const { isOpen, componentKey, closeModal } = useModalStore();
   if (!isOpen || !componentKey) return null;
 
   const Component = componentRegistry[componentKey];
 
   return (
-    <div className="absolute">
-      <Component></Component>
+    <div id="chan-modal">
+      <div className={`${styles['modal-mask']}`} onClick={closeModal}>
+        {/* 这是遮罩层 */}
+      </div>
+      <div className={`${styles['chan-modal']}`}>
+        <Component></Component>
+        <button onClick={closeModal} className={styles['modal-close']}>
+          <Icon className='f' icon="line-md:close-small" width={28} height={28}></Icon>
+        </button>
+      </div>
     </div>
   );
 };
