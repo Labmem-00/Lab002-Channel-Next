e0a8becb6619b1dd87044411fdcbb0b5cc15c995
Labmem.00 <99648872+Labmem-00@users.noreply.github.com>
2024-12-21 18:27:44 +0800

    feat(headerBar): login

--- app/components/Home/Modal/index.tsx
+++ app/components/Home/Modal/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useModalStore } from '@/store/temp/modal';
 import componentRegistry from './registry';
 import styles from './modal.module.scss';
@@ -8,19 +8,41 @@ import { Icon } from '@iconify/react/dist/iconify.js';
 
 const Modal: React.FC = () => {
   const { isOpen, componentKey, closeModal } = useModalStore();
+  const [isVisiable, setVisiable] = useState(true);
+
+  useEffect(() => {
+    if (isVisiable == true) return;
+    const timer = setTimeout(() => {
+      closeModal();
+      setVisiable(true);
+    }, 400);
+
+    return () => clearTimeout(timer);
+  }, [isVisiable]);
+
   if (!isOpen || !componentKey) return null;
 
   const Component = componentRegistry[componentKey];
 
   return (
     <div id="chan-modal">
-      <div className={`${styles['modal-mask']}`} onClick={closeModal}>
+      <div
+        className={`${styles['modal-mask']} ${isVisiable ? '' : styles['mask-out']}`}
+        onClick={closeModal}
+      >
         {/* 这是遮罩层 */}
       </div>
-      <div className={`${styles['chan-modal']}`}>
+      <div
+        className={`${styles['chan-modal']} ${isVisiable ? '' : styles['modal-out']}`}
+      >
         <Component></Component>
-        <button onClick={closeModal} className={styles['modal-close']}>
-          <Icon className='f' icon="line-md:close-small" width={28} height={28}></Icon>
+        <button
+          onClick={() => {
+            setVisiable(false);
+          }}
+          className={styles['modal-close']}
+        >
+          <Icon icon="line-md:close-small" width={28} height={28}></Icon>
         </button>
       </div>
     </div>
