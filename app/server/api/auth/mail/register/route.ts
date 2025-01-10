333d0a7dea74857ec09c41b20b2a9f37b6b0e649
Labmem.00 <99648872+Labmem-00@users.noreply.github.com>
2025-01-10 17:12:00 +0800

    feat(Register): add regiser api

--- app/server/api/auth/mail/register/route.ts
+++ app/server/api/auth/mail/register/route.ts
@@ -1,15 +1,14 @@
+import { NextResponse } from 'next/server';
 import { sendValidCodeMail } from '@/server/utils/sendVaildCode';
 
 export async function POST(request: Request) {
   const { email } = await request.json();
   const result = await sendValidCodeMail(request, email, 'register');
+
   if (result) {
-    return new Response(JSON.stringify({ message:'yes'}), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return NextResponse.json({ message: 'yes' });
   } else {
-    return new Response(JSON.stringify({ message: 'no' }), {
-      headers: { 'Content-Type': 'application/json' },
-    });
+    return NextResponse.json({ message: 'no' });
   }
 }
+
